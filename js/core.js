6349bf39a308d977ba5b5ca18f5e9f079d954f85
vlanesvit <vlanesvit@gmail.com>
2021-09-11 17:28:19 +0300

    udpt

--- js/core.js
+++ js/core.js
@@ -1,130 +1,267 @@
-// creating matrix
-function getMartix(columns, rows) {
+// создание массива матрицы
+function getMatrix (columns, rows) {
   const matrix = [];
 
   let idCounter = 1;
 
   for (let y = 0; y < rows; y++) {
-    const row = [];
+      const row = [];
 
-    for (let x = 0; x < columns; x++) {
-      row.push({
-        id: idCounter++,
-        show: true,
-        flag: false,
-        mine: false,
-        number: 0,
-        x: x,
-        y: y,
-      });
-    }
+      for (let x = 0; x < columns; x++) {
+          row.push({
+              id: idCounter++,
+              left: false,
+              right: false,
+              show: false,
+              flag: false,
+              mine: false,
+              potencial: false,
+              number: 0,
+              x,
+              y
+          })
+      }
 
-    matrix.push(row);
+      matrix.push(row);
   }
 
   return matrix;
 }
 
-// returns free cell
-function getRandomFreeCell(matrix) {
+// возврат свободной клетки
+function getRandomFreeCell (matrix) {
   const freeCells = [];
 
   for (let y = 0; y < matrix.length; y++) {
-    for (let x = 0; x < matrix[y].length; x++) {
-      const cell = matrix[y][x];
+      for (let x = 0; x < matrix[y].length; x++) {
+          const cell = matrix[y][x];
 
-      if (!cell.mine) {
-        freeCells.push(cell);
+          if (!cell.mine) {
+              freeCells.push(cell);
+          }
       }
-    }
   }
 
   const index = Math.floor(Math.random() * freeCells.length);
   return freeCells[index];
 }
 
-// spawn mines
-function setRandomMine(matrix) {
+// появление мин
+function setRandomMine (matrix) {
   const cell = getRandomFreeCell(matrix);
   cell.mine = true;
 
-  // request for neighboring cells
+  // запрос на соседние клетки
   const cells = getAroundCells(matrix, cell.x, cell.y);
+
   for (const cell of cells) {
-    cell.number++;
+      cell.number++;
   }
 }
 
-// returns the cell value
-function getCell(matrix, x, y) {
-  if (!matrix[y] || !matrix[y][x]) {
-    return false;
+// возврат значения клетки
+function getCell (matrix, x, y) {
+  if(!matrix[y] || !matrix[y][x]) {
+      return false;
   }
 
-  return matrix[y][x];
+  return matrix[y][x]
 }
 
-// around the cell
-function getAroundCells(matrix, x, y) {
+// мин вокруг клетки
+function getAroundCells (matrix, x, y) {
   const cells = [];
-
+  
   for (let dx = -1; dx <= 1; dx++) {
-    for (let dy = -1; dy <= 1; dy++) {
-      if (dx === 0 && dy === 0) {
-        continue;
-      }
+      for (let dy = -1; dy <= 1; dy++) {
+          if (dx == 0 && dy == 0) {
+              continue;
+          }
 
-      const cell = getCell(matrix, x + dx, y + dy);
+          const cell = getCell(matrix, x + dx, y + dy);
 
-      if (cell) {
-        cells.push(cell);
+          if (!cell) {
+              continue;
+          }
+
+          cells.push(cell);
       }
-    }
   }
 
   return cells;
 }
 
-// visible "sapper" in browser
-function matrixToHtml(matrix) {
-  const gameElement = document.createElement("div");
-  gameElement.classList.add("sapper");
+// возврат клетки с индексом
+function getCellById (matrix, id) {
+  for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length;x++) {
+          const cell = matrix[y][x] 
+
+          if (cell.id === id) {
+              return cell;
+          }
+      }
+  }
+  return false;
+}
+
+// графическая часть
+function matrixToHtml (matrix) {
+  const gameElement = document.createElement('div');
+  gameElement.classList.add('sapper');
 
   for (let y = 0; y < matrix.length; y++) {
-    const rowElement = document.createElement("div");
-    rowElement.classList.add("row");
+      const rowElement = document.createElement('div');
+      rowElement.classList.add('row');
 
-    for (let x = 0; x < matrix[y].length; x++) {
-      const cell = matrix[y][x];
-      const imgElement = document.createElement("img");
-      imgElement.draggable = false;
+      for (let x = 0; x < matrix[y].length; x++) {
+          const cell = matrix[y][x];
+          const imgElement = document.createElement('img');
 
-      rowElement.append(imgElement);
+          imgElement.draggable = false;
+          imgElement.oncontextmenu = () => false;
+          imgElement.setAttribute('data-cell-id', cell.id)
+          rowElement.append(imgElement);
 
-      if (cell.flag) {
-        imgElement.src = "./assets/svg/flag.svg";
-        continue;
+          if (cell.flag) {
+              imgElement.src = 'assets/svg/flag.svg';
+              continue;
+          }
+
+          if (cell.potencial) {
+              imgElement.src = 'assets/svg/poten.svg';
+              continue;
+          }
+
+          if (!cell.show) {
+              imgElement.src = 'assets/svg/none.svg';
+              continue;
+          }
+
+          if (cell.mine) {
+              imgElement.src = 'assets/svg/bomb.svg';
+              continue;
+          }
+
+          if (cell.number) {
+              imgElement.src = 'assets/svg/' + cell.number + '.svg';
+              continue;
+          }
+
+          imgElement.src = 'assets/svg/free.svg';
+          
       }
 
-      if (!cell.show) {
-        imgElement.src = "./assets/svg/none.svg";
-        continue;
-      }
-
-      if (cell.mine) {
-        imgElement.src = "./assets/svg/bomb.svg";
-        continue;
-      }
-
-      if (cell.number) {
-        imgElement.src = "./assets/svg/" + cell.number + ".svg";
-        continue;
-      }
-
-      imgElement.src = "./assets/svg/none.svg";
-    }
-    gameElement.append(rowElement);
+      gameElement.append(rowElement);
   }
 
   return gameElement;
+}
+
+// проверка прожатой кнопки
+function forEach (matrix, handler) {
+  for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length;x++) {
+          handler(matrix[y][x]);
+      }
+  }
+}
+
+// открытие свободных рядом клеток
+function openSpace (matrix, x, y) {
+  const cell = getCell(matrix, x, y);
+
+  if (cell.flag || cell.number || cell.mine) {
+      return;
+  }
+
+  forEach(matrix, x => x._marked = false);
+
+  cell._marked = true;
+
+  let flag = true;
+  while (flag) {
+      flag = false;
+
+      for (let y = 0; y < matrix.length; y++) {
+          for (let x = 0; x < matrix[y].length;x++){
+              const cell = matrix[y][x];
+
+              if (!cell._marked || cell.number) {
+                  continue;
+              }
+
+              const cells = getAroundCells(matrix, x, y);
+              for (const cell of cells) {
+                  if (cell._marked) {
+                      continue;
+                  }
+
+                  if (!cell.flag && !cell.mine) {
+                      cell._marked = true;
+                      flag = true;
+                  }
+              }
+          }
+      }
+  }
+
+  forEach(matrix, x => {
+      if (x._marked) {
+          x.show = true;
+      }
+
+      delete x._marked});
+}
+
+// победа
+function isWin (matrix) {
+  const flags = [];
+  const mines = [];
+
+  forEach(matrix, cell => {
+      if (cell.flag) {
+          flags.push(cell);
+      }
+
+      if (cell.mine) {
+          mines.push(cell);
+      }
+  })
+
+  if (flags.length !== mines.length) {
+      return false;
+  }
+
+  for (const cell of mines) {
+      if (!cell.flag) {
+          return false;
+      }
+  }
+
+  for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length;x++){
+          const cell = matrix[y][x];
+
+          if(!cell.mine && !cell.show) {
+              return false;
+          }
+      }
+  }
+  
+  return true;
+}
+
+// проигрыш
+function isLoose (matrix) {
+  for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length;x++){
+          const cell = matrix[y][x];
+
+          if(cell.mine && cell.show) {
+              return true;
+          }
+      }
+  }
+
 }
--- js/script.js
+++ js/script.js
@@ -1,11 +1,189 @@
-// creating matrix
-const matrix = getMartix(10, 10)
+let matrix = null;
+let running = null;
 
-// cell with mines
-for (let i = 0; i < 10; i++) {
-  setRandomMine(matrix)
+let width = document.getElementById('width');
+let height = document.getElementById('height');
+let skills = document.getElementsByName('difficulty');
+
+let columns = width.value;
+let rows = height.value;
+let mines = Math.round(width.value * height.value / 15);
+
+width.onchange = function() {
+    columns = width.value;
 }
 
-const gameElement = matrixToHtml(matrix)
-const appElement = document.querySelector('#app')
-appElement.append(gameElement);
+height.onchange = function() {
+    rows = height.value;
+}
+
+for (let i = 0; i < skills.length; i++) {
+    skills[i].onchange = minesNum;
+}
+
+function minesNum() {
+    mines = Math.round(columns * rows / 10 * this.value);
+}
+
+
+init(columns, rows, mines);
+
+  // кнопка рестарт
+  document
+      .querySelector('.restart')
+      .addEventListener('click', () => init(columns, rows, mines));
+
+// инициализация игры
+function init (columns, rows, mines) {
+    matrix = getMatrix(columns, rows);
+    running = true;
+
+    for (let i = 0; i < mines; i++) {
+        setRandomMine(matrix);
+    }
+
+    update();
+}
+
+// обновление графической части
+function update () {
+    if (!running) {
+        return;
+    }
+
+    const gameElement = matrixToHtml(matrix);
+
+    const appElement = document.querySelector('#app');
+    appElement.innerHTML = '';
+    appElement.append(gameElement);
+
+    appElement
+        .querySelectorAll('img')
+        .forEach(imgElement => {
+            imgElement.addEventListener('mousedown', mousedownHandler);
+            imgElement.addEventListener('mouseup', mouseupHandler);
+            imgElement.addEventListener('mouseleave', mouseleaveHandler)
+        });
+
+    if (isLoose(matrix)) {
+        alert('Вы проиграли :(')
+        running = false;
+    } else if (isWin(matrix)) {
+        alert('Вы победили!')
+        running = false;
+    }
+}
+
+// прожатие кнопки
+function mousedownHandler (event) {
+    const {cell, left, right } = getInfo(event);
+
+    if (left) {
+        cell.left = true;
+    }
+
+    if (right) {
+        cell.right = true;
+    }
+
+    if (cell.left && cell.right) {
+        bothHandler(cell);
+    }
+
+    update();
+}
+
+// отжатие кнопки
+function mouseupHandler (event) {
+    const {left, right, cell } = getInfo(event);
+
+    const both = cell.right && cell.left && (left || right);
+    const leftMouse = !both && cell.left && left;
+    const rightMouse = !both && cell.right && right;
+
+    if (both) {
+        forEach(matrix, x => x.potencial = false);
+    }
+
+    if (left) {
+        cell.left = false;
+    }
+
+    if (right) {
+        cell.right = false;
+    }
+
+    if (leftMouse) {
+        leftHandler(cell)
+    }
+
+    else if (rightMouse) {
+        rightHandler(cell)
+    }
+
+    update();
+}
+
+// потеря курсора
+function mouseleaveHandler (event) {
+    const info = getInfo(event);
+
+        info.cell.left = false;
+        info.cell.right = false;
+
+    update();
+}
+
+// возврат клетки, прожатой кнопкой
+function getInfo (event) {
+    const element = event.target;
+    const cellId = parseInt(element.getAttribute('data-cell-id'));
+
+    return {
+        left: event.which === 1,
+        right: event.which === 3,
+        cell: getCellById(matrix, cellId)
+    }
+}
+
+// левая кнопка мыши
+function leftHandler (cell) {
+    if (cell.show || cell.flag) {
+        return;
+    }
+    cell.show = true;
+
+    openSpace(matrix, cell.x, cell.y);
+}
+
+// правая кнопка мыши
+function rightHandler (cell) {
+    if (!cell.show) {
+         cell.flag = !cell.flag;
+    }
+}
+
+// обе кнопки мыши
+function bothHandler (cell) {
+    if (!cell.show || !cell.number) {
+        return;
+    }
+
+    const cells = getAroundCells(matrix, cell.x, cell.y);
+    const flags = cells.filter(x => x.flag).length;
+
+    if (flags === cell.number) {
+        cells
+            .filter(x => !x.flag && !x.show)
+            .forEach(cell => {
+                cell.show = true
+                openSpace(matrix, cell.x, cell.y)
+            })
+    }
+
+    else {
+        cells
+            .filter(x => !x.flag && !x.show)
+            .forEach(cell => cell.potencial = true)
+    }
+}
